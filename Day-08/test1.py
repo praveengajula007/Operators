996207a5821e13218b5f28dfe0c9e4fe35aa9e35
praveengajula007 <47707777+praveengajula007@users.noreply.github.com>
2023-11-08 07:19:03 +0000

    Lists

---
+++ Day-08/test1.py
@@ -0,0 +1,12 @@
+#my_tuple = (1, 2, 'apple', 'banana')
+
+#is_present = 'apple' in my_tuple
+#new_tuple = my_tuple + (3.14, 'cherry')
+#coordinates = (3, 4)
+#x, y = coordinates  # Unpack the tuple into x and y (x=3, y=4)
+def get_coordinates():
+    return (3, 4)
+
+x, y = get_coordinates() 
+print(get_coordinates())
+
