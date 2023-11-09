7b4602179edb1f9032c5f34b621236f030684895
praveengajula007 <47707777+praveengajula007@users.noreply.github.com>
2023-11-09 05:54:56 +0000

    Error

--- Day-09/break-continue.py
+++ Day-09/break-continue.py
@@ -1,11 +1,25 @@
+#Break statement
 numbers = [1,2,3,4,5,6,7,8,9]
 for i in numbers :
     if i == 2:
         break
     print(i)
 
+#Continue statement
 #numbers = [1,2,3,4,5,6,7,8,9]
 for i in numbers :
     if i == 2:
         continue
     print(i)
+
+# Automating Log File Analysis
+log_file = [
+   "INFO: Operation successful",
+   "ERROR: File not found",
+   "DEBUG: Connection established",
+   "ERROR: Database connection failed",
+]
+
+for line in log_file:
+    if "ERROR" in line:
+        print(line)
