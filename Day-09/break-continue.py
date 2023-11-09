8080fc0fd111851f70f651121caca490e1e7665b
praveengajula007 <47707777+praveengajula007@users.noreply.github.com>
2023-11-09 05:46:53 +0000

    break-continue

---
+++ Day-09/break-continue.py
@@ -0,0 +1,11 @@
+numbers = [1,2,3,4,5,6,7,8,9]
+for i in numbers :
+    if i == 2:
+        break
+    print(i)
+
+#numbers = [1,2,3,4,5,6,7,8,9]
+for i in numbers :
+    if i == 2:
+        continue
+    print(i)
